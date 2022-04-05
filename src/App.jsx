d21916bd7561d76566a9f6932eca8bf4b7f3b5a1
Peter Joachym <joachym.peter@gmail.com>
2022-04-05 11:23:41 +0200

    add: import of dinner and welcome pages to App.jsx done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import WelcomePage from './pages/WelcomePage/WelcomePage';
+import DinnerPage from './pages/DinnerPage/DinnerPage';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomePage/>
+      <DinnerPage/>  
+    </div>
+  );
+}
+
+export default App;
