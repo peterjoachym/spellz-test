3df5e449b0a0604bec7607944c8385c59c89f534
Peter Joachym <joachym.peter@gmail.com>
2022-04-05 13:35:03 +0200

    add: wwelcome page logo added and centered

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='logo__container'>
+        <img className='logo__content' src='/assets/logo_spellz_white.png' alt='logo__spellz' />
+    </div>
+  )
+}
+
+export default Logo
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./WelcomePage.css";
+import Logo from "../../components/Logo/Logo";
 
 const WelcomePage = () => {
-  return <div className="welcome__page__container">WelcomePage</div>;
+  return <div className="welcome__page__container">
+      <Logo/>
+      </div>;
 };
 
 export default WelcomePage;
