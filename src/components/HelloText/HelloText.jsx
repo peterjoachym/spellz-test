611260018953a7b89cb41f230a7a784ddd825720
Peter Joachym <joachym.peter@gmail.com>
2022-04-05 14:48:46 +0200

    add: hello text added and css adjusted

---
+++ src/components/HelloText/HelloText.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HelloText = () => {
+  return (
+    <div className="hello__text__container">
+      <h1 className="hello__content">
+        Yo 👋 <br />
+        <br />
+        <br />
+        What’s your <br />
+        <br />
+        first name ?
+      </h1>
+    </div>
+  );
+};
+
+export default HelloText;
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import "./WelcomePage.css";
 import Logo from "../../components/Logo/Logo";
+import HelloText from "../../components/HelloText/HelloText";
 
 const WelcomePage = () => {
   const [shrink, setShrink] = useState(false);
   return (
     <div className="welcome__page__container">
       <Logo shrink={shrink} setShrink={setShrink}/>
+      <HelloText shrink={shrink}/>
     </div>
   );
 };
