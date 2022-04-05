c8614c3cc88b23e2e4c6ced96698475e4f8b2890
Peter Joachym <joachym.peter@gmail.com>
2022-04-05 11:53:42 +0200

    add: navigation established

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css';
-import WelcomePage from './pages/WelcomePage/WelcomePage';
-import DinnerPage from './pages/DinnerPage/DinnerPage';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
+import DinnerPage from "./pages/DinnerPage/DinnerPage";
 
 function App() {
   return (
     <div className="App">
-      <WelcomePage/>
-      <DinnerPage/>  
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/dinner" element={<DinnerPage />} />
+      </Routes>
     </div>
   );
 }
