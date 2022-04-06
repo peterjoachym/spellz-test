d673b9f01647c3459e6f0fb49cc4fe4a571463e6
Peter Joachym <joachym.peter@gmail.com>
2022-04-06 12:06:10 +0200

    add: AddItem button added and css adjusted

--- src/components/AddItemButton/AddItemButton.jsx
+++ src/components/AddItemButton/AddItemButton.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const AddItemButton = () => {
   return (
-    <div>AddItemButton</div>
-  )
-}
+    <div className="add__item__button__container">
+      <button className="add__item__button" type="button" onClick="">
+        + Add Item
+      </button>
+    </div>
+  );
+};
 
-export default AddItemButton
+export default AddItemButton;
--- src/pages/DinnerPage/DinnerPage.jsx
+++ src/pages/DinnerPage/DinnerPage.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import DinnerHeader from "../../components/DinnerHeader/DinnerHeader";
 import "./DinnerPage.css";
 import Items from "../../components/Items/Items";
+import AddItemButton from "../../components/AddItemButton/AddItemButton";
 
 const DinnerPage = () => {
-  return <div className="dinner__page__container">
-    <DinnerHeader/>
-    <Items/>
-  </div>;
+  return (
+    <div className="dinner__page__container">
+      <DinnerHeader />
+      <Items />
+      <AddItemButton />
+    </div>
+  );
 };
 
 export default DinnerPage;
