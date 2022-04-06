35c9ecebdca75f0a8b06bb50ebdb14713a4ac36d
Peter Joachym <joachym.peter@gmail.com>
2022-04-07 00:37:42 +0200

    fix: user input fixed

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -4,22 +4,29 @@ import "./WelcomePage.css";
 import Logo from "../../components/welcomePageComponents/Logo/Logo";
 import HelloText from "../../components/welcomePageComponents/HelloText/HelloText";
 import NameInput from "../../components/welcomePageComponents/NameInput/NameInput";
-import WelcomePageButtons from "../../components/welcomePageComponents/WelcomePageButtons/WelcomePageButtons";
+// import WelcomePageButtons from "../../components/welcomePageComponents/WelcomePageButtons/WelcomePageButtons";
+// import userDinnerContext from "../../context/userDinnerContext";
 
 const WelcomePage = () => {
+  // const { setUser } = useContext(userDinnerContext);
   const [shrink, setShrink] = useState(false);
 
   const navigate = useNavigate();
+  
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+    // setUser(firstName);
     navigate("/dinner");
   };
   return (
     <div className="welcome__page__container">
       <Logo shrink={shrink} setShrink={setShrink} />
       <HelloText shrink={shrink} />
-      <NameInput shrink={shrink} />
-      <WelcomePageButtons shrink={shrink} handleSubmit={handleSubmit} />
+      <NameInput shrink={shrink}  handleSubmit={handleSubmit}/>
+      {/* <WelcomePageButtons shrink={shrink} 
+      // handleSubmit={handleSubmit} 
+      /> */}
     </div>
   );
 };
